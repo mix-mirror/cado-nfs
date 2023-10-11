d09899b186d43e9c5165a72e089a5aa455058445
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2023-10-11 13:47:52 +0200

    improve test for double matrix.
    Apparently the test case that I identified yields an endless loop with thr22.
    (it is probably so as a consequence of having set the permutations right)

--- tests/linalg/bwc/bwc-test-double-matrix.sh
+++ tests/linalg/bwc/bwc-test-double-matrix.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-tmp=$(mktemp -d /tmp/cado-nfs.XXXXXXXXXXXXXX)
+tmp=$(mktemp -d /tmp/cado-nfs.bwc-test-double-matrix.XXXXXXXXXXXXXX)
 
 if ! [ "$CADO_DEBUG" ] ; then
     trap "rm -rf $tmp" EXIT
@@ -17,7 +17,7 @@ usage() {
     exit 1
 }
 
-TESTCASE="123 251 172 STRETCH STRETCH"  # for thr=2x2
+# TESTCASE="123 251 172 STRETCH STRETCH"  # for thr=2x2
 TESTCASE="159 193 129 STRETCH STRETCH"  # for thr=1x1
 bindir="$CMAKE_CURRENT_BINARY_DIR"
 thr=2x2
@@ -56,14 +56,33 @@ for m in "$@" ; do
     matrices="$matrices$m"
 done
 
+pmn=(
+    prime=2
+    m=64
+    n=64
+)
+
+eval "${pmn[@]}"
+
 common=(
         multi_matrix=1
         matrix=$matrices
+        "${pmn[@]}"
         wdir=$tmp
-        mn=64
         balancing_options=reorder=none,skip_decorrelating_permutation=1
         thr=$thr
     )
 $bindir/prep "${common[@]}" seed=$seed
-$bindir/secure "${common[@]}" interval=1
-
+$bindir/secure "${common[@]}"
+$bindir/krylov "${common[@]}" ys=0..$n
+$bindir/acollect wdir=$tmp "${pmn[@]}" remove_old=1
+afiles=(`find $tmp -name "A0-*.0-*"`)
+if [ "${#afiles[@]}" != 1 ] ; then
+    echo "Error: want exactly one A file for lingen" >&2
+    exit 1
+fi
+afile="${afiles[0]}"
+ffile="$afile.gen"
+$bindir/lingen_u64k1 "${pmn[@]}" wdir=$tmp afile="$afile" ffile=F rhs=none split-output-file=1
+$bindir/mksol "${common[@]}" solutions=0-$n
+$bindir/gather "${common[@]}" solutions=0-$n
