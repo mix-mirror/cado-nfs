5a3199aaa926588df825b334244f63d170ad07e0
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2023-11-15 09:35:55 +0100

    added script for #30070

---
+++ tests/linalg/bwc/bwc-test-double-matrix_2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# https://gitlab.inria.fr/cado-nfs/cado-nfs/-/issues/30070
+p=`pwd`
+d=`mktemp -d`
+cd $d
+wget https://gitlab.inria.fr/cado-nfs/cado-nfs/uploads/d25129cf3974d86e7a0fd10fd71ced1b/R.rw.bin
+wget https://gitlab.inria.fr/cado-nfs/cado-nfs/uploads/31d04463ed3d8b281bb199d35a96928d/R.dense.rw.bin
+wget https://gitlab.inria.fr/cado-nfs/cado-nfs/uploads/ae1220c9cfff6e0fda2058bb29a610d1/R.dense.cw.bin
+wget https://gitlab.inria.fr/cado-nfs/cado-nfs/uploads/fe42bdf5ed7111df16e4ff5c9636c797/R.dense.bin
+wget https://gitlab.inria.fr/cado-nfs/cado-nfs/uploads/9cad98eb94b0b0f58c83929859ab2f9b/R.cw.bin
+wget https://gitlab.inria.fr/cado-nfs/cado-nfs/uploads/844849ec8988228dd9ebc41b60ac86b5/R.bin
+wget https://gitlab.inria.fr/cado-nfs/cado-nfs/uploads/d2325acbe43b04b0cbca7def5c5968d1/L.rw.bin
+wget https://gitlab.inria.fr/cado-nfs/cado-nfs/uploads/76752bc10757f9f97483e42a6f5db363/L.cw.bin
+wget https://gitlab.inria.fr/cado-nfs/cado-nfs/uploads/fd79194a888208fb667606878d5c429b/L.bin
+echo $d
+cd $p
+./build/`hostname`/linalg/bwc/prep multi_matrix=1 matrix=$d/L.bin,$d/R.bin wdir=$d/bwc mn=64 balancing_options=reorder=columns thr=2x2
+./build/`hostname`/linalg/bwc/secure multi_matrix=1 matrix=$d/L.bin,$d/R.bin wdir=$d/bwc mn=64 thr=1x1 prime=2 balancing_options=reorder=none,skip_decorrelating_permutation=1 
+./build/`hostname`/linalg/bwc/krylov multi_matrix=1 matrix=$d/L.bin,$d/R.bin wdir=$d/bwc mn=64 thr=1x1 prime=2 balancing_options=reorder=none,skip_decorrelating_permutation=1 ys=0..64
+./build/`hostname`/linalg/bwc/acollect wdir=$d/bwc mn=64 prime=2 ys=0..64 remove_old=1
+
