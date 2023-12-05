1265d918460b72b906dbd4610b1ab4ac5e10d6e1
Charles Bouillaguet <charles.bouillaguet@lip6.fr>
2023-12-05 06:13:51 +0100

    prettier comments

--- filter/merge_heap.h
+++ filter/merge_heap.h
@@ -49,31 +49,44 @@ void heap_setup(heapctx_t ctx);
 
 int heap_config_get_PAGE_DATA_SIZE();
 
-/* Allocate space for row i, holding s coefficients in row[1:s+1] (row[0] == s). 
-	This writes s in row[0].  The size of the row must not change afterwards.
-	Thread-safe.  Usually very fast. */
+/*
+ * Allocate space for row i, holding s coefficients in row[1:s+1] (row[0] == s).
+ * This writes s in row[0].  The size of the row must not change afterwards.
+ * Thread-safe.  Usually very fast.
+ */
 typerow_t * heap_alloc_row (heapctx_t ctx, index_t i, size_t s);
 
-/* Shrinks the last allocated row.  It must be the last one allocated by this
-	thread. Lock-free and constant-time. */
+/*
+ * Shrinks the last allocated row.  It must be the last one allocated by this
+ * thread. Lock-free and constant-time.
+ */
 void heap_resize_last_row (heapctx_t ctx, typerow_t *row, index_t new_size);
 
-/* Given the pointer provided by heap_alloc_row, mark the row as deleted.
-	The memory is not released.  Lock-free and constant-time. */
+/*
+ * Given the pointer provided by heap_alloc_row, mark the row as deleted.
+ * The memory is not released.  Lock-free and constant-time.
+ */
 void heap_destroy_row (heapctx_t ctx, typerow_t *row);
 
-/* Reclaim all the memory occupied by destroyed rows. 
-	Must be called outside of a parallel region.  May move rows in memory and update row pointers. */
+/*
+ * Reclaim all the memory occupied by destroyed rows.
+ * Must be called outside of a parallel region.
+ * May move rows in memory and update row pointers (in rows).
+ */
 void heap_garbage_collection(heapctx_t ctx, typerow_t **rows);
 
-/* Deallocate all rows. Does not release memory (it remains available for row allocation) */
+/*
+ * Deallocate all rows.
+ * Does not release memory (it remains available for row allocation)
+ */
 void heap_reset (heapctx_t ctx);
 
-/* release all memory. This is technically not necessary, because the "malloc"
-	allocations are internal to the process, and all space allocated to the
-	process is reclaimed by the OS on termination. However, doing this enables
-	valgrind to check the absence of leaks.
-*/
+/*
+ * Release all memory. This is technically not necessary, because the "malloc"
+ * allocations are internal to the process, and all space allocated to the
+ * process is reclaimed by the OS on termination. However, doing this enables
+ * valgrind to check the absence of leaks.
+ */
 void heap_clear (heapctx_t ctx);
 
 #ifdef __cplusplus
