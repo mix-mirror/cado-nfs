574eebc367efbbb9007122d51ed427d12d6641d9
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2025-08-07 16:49:23 +0200

    cover cxx_{mpfr,mpc} as well as {mpfr,mpc}_{aux.h,auxx.hpp}
    In passing, fixing a few interface glitches.
    
    The implicit conversions definitely do some harm, because they lead to
    some ambiguous resolutions of operators if we're not careful. It is also
    debatable that we really want to have both the C and C++ types as
    primary citizens.

--- utils/cxx_mpz.hpp
+++ utils/cxx_mpz.hpp
@@ -13,9 +13,9 @@
 #include "fmt/ostream.h"
 #include "fmt/base.h"
 
-#include "is_non_narrowing_conversion.hpp"
 #include "gmp_aux.h"
 #include "gmp_auxx.hpp"
+#include "utils_cxx.hpp"
 #include "macros.h"
 
 /* TODO: This uses enable_if_t, which is in fact c++14.
@@ -29,21 +29,16 @@ public:
     // NOLINTBEGIN(cppcoreguidelines-pro-type-member-init,hicpp-member-init)
     cxx_mpz() { mpz_init(x); }
 
-    template<typename T, typename U>
-    static constexpr bool converts_via =
-        integral_fits_v<T, U> &&
-        cado_math_aux::is_non_narrowing_conversion_v<T, U>;
-
     template <typename T>
         // NOLINTNEXTLINE(hicpp-explicit-conversions)
         cxx_mpz (const T & rhs)
-        requires converts_via<T, int64_t>
+        requires cado::converts_via<T, int64_t>
         {
             gmp_auxx::mpz_init_set(x, int64_t(rhs));
         }
     template <typename T>
         cxx_mpz & operator=(const T a)
-        requires converts_via<T, int64_t>
+        requires cado::converts_via<T, int64_t>
         {
             gmp_auxx::mpz_set(x, int64_t(a));
             return *this;
@@ -51,13 +46,13 @@ public:
     template <typename T>
         // NOLINTNEXTLINE(hicpp-explicit-conversions)
         cxx_mpz (const T & rhs)
-        requires converts_via<T, uint64_t>
+        requires cado::converts_via<T, uint64_t>
         {
             gmp_auxx::mpz_init_set(x, uint64_t(rhs));
         }
     template <typename T>
         cxx_mpz & operator=(const T a)
-        requires converts_via<T, uint64_t>
+        requires cado::converts_via<T, uint64_t>
         {
             gmp_auxx::mpz_set(x, uint64_t(a));
             return *this;
@@ -222,7 +217,7 @@ CXX_MPZ_DEFINE_CMP(>)
 CXX_MPZ_DEFINE_CMP(<=)
 CXX_MPZ_DEFINE_CMP(>=)
 
-inline bool operator<=>(cxx_mpz const & a, cxx_mpz const & b) { return gmp_auxx::mpz_cmp(a, b); }
+inline int operator<=>(cxx_mpz const & a, cxx_mpz const & b) { return gmp_auxx::mpz_cmp(a, b); }
 
 inline cxx_mpz operator+(cxx_mpz const & a, cxx_mpz const & b) { cxx_mpz r; mpz_add(r, a, b); return r; }
 template <typename T>
