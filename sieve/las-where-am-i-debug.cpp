mix-mirror/cado-nfs

commit 96bc43c3bd2190258978d0cc7ff7ad72661bfa36

sieve: continue to adapt trace mechanism to large values of a

sieve/las-where-am-i-debug.cpp
@@ -49,7 +49,11 @@
 #define TRACE_K 1
 #endif
 
+#ifdef SUPPORT_LARGE_Q
+int extern_trace_on_spot_ab(cxx_mpz const & a, cxx_mpz const & b) {
+#else
 int extern_trace_on_spot_ab(int64_t a, uint64_t b) {
+#endif
     return trace_on_spot_ab(a, b);
 }
 

sieve/las-where-am-i-debug.hpp
@@ -105,7 +105,11 @@ static inline int trace_on_spot_x(uint64_t x) {
         + (uint64_t)trace_Nx.x;
 }
 
+#ifdef SUPPORT_LARGE_Q
+static inline int trace_on_spot_ab(cxx_mpz const & a, cxx_mpz const & b) {
+#else
 static inline int trace_on_spot_ab(int64_t a, uint64_t b) {
+#endif
     return a == trace_ab.a && b == trace_ab.b;
 }
 

sieve/las-where-am-i-prod.cpp
@@ -23,7 +23,11 @@ struct cxx_param_list; // IWYU pragma: keep
 class nfs_work; // IWYU pragma: keep
 
 
+#ifdef SUPPORT_LARGE_Q
+int extern_trace_on_spot_ab(cxx_mpz const &, cxx_mpz const &) {
+#else
 int extern_trace_on_spot_ab(int64_t, uint64_t) {
+#endif
     return 0;
 }
 

sieve/las-cofac-standalone.cpp
@@ -54,7 +54,11 @@ cofac_standalone::cofac_standalone(int nsides, int N, size_t x, int logI,
 #endif
 }/*}}}*/
 bool cofac_standalone::trace_on_spot() const {/*{{{*/
+#ifdef SUPPORT_LARGE_Q
+    return extern_trace_on_spot_ab(az, bz);
+#else
     return extern_trace_on_spot_ab(a, b);
+#endif
 }/*}}}*/
 bool cofac_standalone::gcd_coprime_with_q(special_q const & E) const {/*{{{*/
     /* Since the q-lattice is exactly those (a, b) with
@@ -160,3 +164,11 @@ int cofac_standalone::factor_leftover_norms(nfs_work_cofac & wc) {/*{{{*/
             *wc.strategies);
 }/*}}}*/
 
+std::ostream& operator<<(std::ostream& os, cofac_standalone const & cur)
+{
+#ifdef SUPPORT_LARGE_Q
+    return os << "(" << cur.az << ", " << cur.bz << ")";
+#else
+    return os << "(" << cur.a << ", " << cur.b << ")";
+#endif
+}

sieve/las-cofac-standalone.hpp
@@ -4,8 +4,11 @@
 #include <cstdio>
 #include <cstdint>
 
+#include <iostream>
 #include <list>
 #include <vector>
+
+#include "fmt/ostream.h"
 
 #include "cxx_mpz.hpp"
 #include "ecm/batch.hpp"
@@ -55,7 +58,13 @@ struct cofac_standalone {
     relation get_relation(special_q const & doing) const;
     void transfer_to_cofac_list(lock_guarded_container<std::list<cofac_candidate>> & L);
     int factor_leftover_norms(nfs_work_cofac & wc);
+    friend std::ostream& operator<<(
+            std::ostream& os,
+            cofac_standalone const & cur);
 };
 
+namespace fmt {
+    template <> struct formatter<cofac_standalone>: ostream_formatter {};
+}
 
 #endif	/* CADO_LAS_COFAC_STANDALONE_HPP */

sieve/las-divide-primes.cpp
@@ -204,11 +204,10 @@ void divide_known_primes(std::vector<uint64_t> & fl, cxx_mpz & norm,
                          bool const handle_2, bucket_primes_t * primes,
                          bucket_array_complete * purged,
                          trialdiv_data const & td, int64_t a, uint64_t b,
-                         fb_factorbase::slicing const & fbs)
+                         fb_factorbase::slicing const & fbs,
+                         bool very_verbose)
 {
-    int const trial_div_very_verbose = extern_trace_on_spot_ab(a, b);
-
-    if (trial_div_very_verbose) {
+    if (very_verbose) {
         verbose_output_start_batch();
         verbose_fmt_print(
             TRACE_CHANNEL, 0,
@@ -223,7 +222,7 @@ void divide_known_primes(std::vector<uint64_t> & fl, cxx_mpz & norm,
         int const bit = mpz_scan1(norm, 0);
         for (int i = 0; i < bit; ++i)
             fl.push_back(2);
-        if (trial_div_very_verbose)
+        if (very_verbose)
             verbose_fmt_print(TRACE_CHANNEL, 0,
                                    "# x = {}, dividing out 2^{}, norm = {}\n",
                                    x, bit, norm);
@@ -231,16 +230,16 @@ void divide_known_primes(std::vector<uint64_t> & fl, cxx_mpz & norm,
     }
 
     // remove primes in "primes" that map to x
-    divide_primes_from_bucket(fl, norm, N, x, primes, trial_div_very_verbose);
+    divide_primes_from_bucket(fl, norm, N, x, primes, very_verbose);
     size_t const nf_divide_primes = fl.size();
 
     // now remove prime hints in "purged". If we had no factor base, then
     // we really should have an empty list here.
     divide_hints_from_bucket(fl, norm, N, x, purged, fbs,
-                             trial_div_very_verbose);
+                             very_verbose);
     size_t const nf_divide_hints = fl.size();
 
-    if (trial_div_very_verbose)
+    if (very_verbose)
         verbose_fmt_print(
             TRACE_CHANNEL, 0,
             "# x = {}, after dividing out bucket/resieved norm = {}\n", x,
@@ -248,7 +247,7 @@ void divide_known_primes(std::vector<uint64_t> & fl, cxx_mpz & norm,
 
     /* Trial divide primes with precomputed tables */
 
-    if (trial_div_very_verbose) {
+    if (very_verbose) {
         auto p = [](auto const & e) { return e.p; };
         verbose_fmt_print(TRACE_CHANNEL, 0, "# Trial division by {}\n",
                fmt::join(td | std::views::transform(p), " "));
@@ -257,7 +256,7 @@ void divide_known_primes(std::vector<uint64_t> & fl, cxx_mpz & norm,
     td.trial_divide(fl, norm);
     size_t const nf_td = fl.size();
 
-    if (trial_div_very_verbose) {
+    if (very_verbose) {
         std::ostringstream os;
         size_t i = 0;
         if (i < nf_divide_primes) {
@@ -285,6 +284,6 @@ void divide_known_primes(std::vector<uint64_t> & fl, cxx_mpz & norm,
                                norm);
     }
 
-    if (trial_div_very_verbose)
+    if (very_verbose)
         verbose_output_end_batch();
 }

sieve/las-divide-primes.hpp
@@ -23,6 +23,7 @@ divide_known_primes (std::vector<uint64_t> & fl, cxx_mpz & norm, const unsigned 
            bucket_array_complete *purged,
 	   trialdiv_data const & td,
            int64_t a, uint64_t b,
-           fb_factorbase::slicing const & fbs);
+           fb_factorbase::slicing const & fbs,
+           bool very_verbose);
 
 #endif	/* CADO_LAS_DIVIDE_PRIMES_HPP */

sieve/las-process-bucket-region.cpp
@@ -572,7 +572,9 @@ void process_bucket_region_run::cofactoring_sync (survivors_t & survivors)/*{{{*
         }
 
         if (cur.trace_on_spot())
-            verbose_fmt_print(TRACE_CHANNEL, 0, "# about to start cofactorization for ({},{})  {} {}\n", cur.a, cur.b, x, Sx[x]);
+            verbose_fmt_print(TRACE_CHANNEL, 0,
+                    "# about to start cofactorization for {} {} {}\n",
+                    cur, x, Sx[x]);
 
         /* since a,b both even were not sieved, either a or b should
          * be odd. However, exceptionally small norms, even without
@@ -637,10 +639,8 @@ void process_bucket_region_run::cofactoring_sync (survivors_t & survivors)/*{{{*
 
                 if (cur.trace_on_spot()) {
                     verbose_fmt_print(TRACE_CHANNEL, 0,
-                            "# start trial division for norm={} ",
-                            cur.norm[side]);
-                    verbose_fmt_print(TRACE_CHANNEL, 0,
-                            "on side {} for ({},{})\n", side, cur.a, cur.b);
+                            "# start trial division for norm={} on side {} "
+                            "for {}\n", cur.norm[side], side, cur);
                 }
 
                 if (wss.no_fb()) {
@@ -664,7 +664,7 @@ void process_bucket_region_run::cofactoring_sync (survivors_t & survivors)/*{{{*
                         &sides[side].purged,
                         *wss.td,
                         cur.a, cur.b,
-                        *wss.fbs);
+                        *wss.fbs, cur.trace_on_spot());
 
                 /* if q is composite, its prime factors have not been sieved.
                  * Check if they divide. They probably don't, since we
@@ -686,9 +686,8 @@ void process_bucket_region_run::cofactoring_sync (survivors_t & survivors)/*{{{*
                 pass = check_leftover_norm (cur.norm[side], ws.conf.sides[side]);
                 if (cur.trace_on_spot()) {
                     verbose_fmt_print(TRACE_CHANNEL, 0,
-                            "# checked leftover norm={} on side {} for "
-                            "({},{}): {}\n",
-                            cur.norm[side], side, cur.a, cur.b, pass);
+                            "# checked leftover norm={} on side {} for {}: {}\n",
+                            cur.norm[side], side, cur, pass);
                 }
                 rep.survivors.check_leftover_norm_on_side[side] += pass;
             }

commit 5ce4e175980c4de698965a98aa60d04aa21c977e

Fix compilation of dupsup broken by 96bc43c3

sieve/las-where-am-i-debug.cpp
@@ -49,11 +49,11 @@
 #define TRACE_K 1
 #endif
 
-#ifdef SUPPORT_LARGE_Q
 int extern_trace_on_spot_ab(cxx_mpz const & a, cxx_mpz const & b) {
-#else
+    return trace_on_spot_ab(a, b);
+}
+
 int extern_trace_on_spot_ab(int64_t a, uint64_t b) {
-#endif
     return trace_on_spot_ab(a, b);
 }
 

sieve/las-where-am-i-debug.hpp
@@ -105,11 +105,11 @@ static inline int trace_on_spot_x(uint64_t x) {
         + (uint64_t)trace_Nx.x;
 }
 
-#ifdef SUPPORT_LARGE_Q
 static inline int trace_on_spot_ab(cxx_mpz const & a, cxx_mpz const & b) {
-#else
+    return a == trace_ab.a && b == trace_ab.b;
+}
+
 static inline int trace_on_spot_ab(int64_t a, uint64_t b) {
-#endif
     return a == trace_ab.a && b == trace_ab.b;
 }
 

sieve/las-where-am-i-prod.cpp
@@ -23,11 +23,11 @@ struct cxx_param_list; // IWYU pragma: keep
 class nfs_work; // IWYU pragma: keep
 
 
-#ifdef SUPPORT_LARGE_Q
 int extern_trace_on_spot_ab(cxx_mpz const &, cxx_mpz const &) {
-#else
+    return 0;
+}
+
 int extern_trace_on_spot_ab(int64_t, uint64_t) {
-#endif
     return 0;
 }
 